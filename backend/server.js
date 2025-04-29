81f7168d13063c0b2ede88e633e2777971b8c37a
Frenkie Wang <frenkiewang21@gmail.com>
2025-04-29 02:10:01 +0100

    test 3

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const userRoutes = require('./routes/userRoutes.js');
-const journeyPlanRoutes = require('./routes/journeyPlanRoutes.js');
-const travelLogRoutes = require('./routes/travelLogRoutes.js');
+// const userRoutes = require('./routes/userRoutes.js');
+// const journeyPlanRoutes = require('./routes/journeyPlanRoutes.js');
+// const travelLogRoutes = require('./routes/travelLogRoutes.js');
 
-app.use('/user', userRoutes);
-app.use('/journeyPlan', journeyPlanRoutes);
-app.use('/travelLog', travelLogRoutes);
-
-// Synchronize the Model into SQL
-sequelize.sync()
-  .then(() => {
-    console.log('Database connected');
-    app.listen(5000, () => console.log('Server started on port 5000'));
-  })
-  .catch(error => console.error(error));
+// app.use('/user', userRoutes);
+// app.use('/journeyPlan', journeyPlanRoutes);
+// app.use('/travelLog', travelLogRoutes);
 
 // Test the Vercel
 app.get("/", (request, response) => {
 	response.send("You succeeded to deploy backend to Vercel!");
 });
+
+// // Synchronize the Model into SQL
+// sequelize.sync()
+//   .then(() => {
+//     console.log('Database connected');
+//     app.listen(5000, () => console.log('Server started on port 5000'));
+//   })
+//   .catch(error => console.error(error));
